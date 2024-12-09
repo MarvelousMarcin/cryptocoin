a8408b6be45140f3997148b7334d6d708285d6f8
Marcin Maciąg <01159356@pw.edu.pl>
2024-12-10 00:23:55 +0100

    some validations

--- src/transactionPool.ts
+++ src/transactionPool.ts
@@ -1,4 +1,4 @@
-import * as _ from "lodash";
+import _ from "lodash";
 import { Transaction, TxIn, UnspentTxOut } from "./classes/Transaction";
 
 let transactionPool: Transaction[] = [];
@@ -11,8 +11,43 @@ export const addToTransactionPool = (
   tx: Transaction,
   unspentTxOuts: UnspentTxOut[]
 ) => {
+  if (!isValidTxForPool(tx, transactionPool)) {
+    throw Error("Trying to add invalid tx to pool");
+  }
+
   console.log("adding to txPool: %s", JSON.stringify(tx));
   transactionPool.push(tx);
+};
+
+const getTxPoolIns = (aTransactionPool: Transaction[]): TxIn[] => {
+  return _(aTransactionPool)
+    .map((tx) => tx.txIns)
+    .flatten()
+    .value();
+};
+
+const isValidTxForPool = (
+  tx: Transaction,
+  aTtransactionPool: Transaction[]
+): boolean => {
+  const txPoolIns: TxIn[] = getTxPoolIns(aTtransactionPool);
+
+  const containsTxIn = (txIns: TxIn[], txIn: TxIn) => {
+    return _.find(txPoolIns, (txPoolIn) => {
+      return (
+        txIn.txOutIndex === txPoolIn.txOutIndex &&
+        txIn.txOutId === txPoolIn.txOutId
+      );
+    });
+  };
+
+  for (const txIn of tx.txIns) {
+    if (containsTxIn(txPoolIns, txIn)) {
+      console.log("txIn already found in the txPool");
+      return false;
+    }
+  }
+  return true;
 };
 
 const hasTxIn = (txIn: TxIn, unspentTxOuts: UnspentTxOut[]): boolean => {
